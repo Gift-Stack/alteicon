d62b9b2bae8c7b1ac7dd2a91472fb8ca0a5c6d77
Gift <gkenny896@gmail.com>
2021-05-01 23:42:56 +0100

    initial commit

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import { IoIosArrowDropdown } from 'react-icons/io';
+
+const Jumbotron = () => {
+    return (
+        <div className='vh'>
+            <h1>ALTEICON</h1>
+            <IoIosArrowDropdown color='#666' size={25} />
+        </div>
+    );
+};
+
+export default Jumbotron;
